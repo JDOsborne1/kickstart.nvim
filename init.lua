e6c58cc12f228676611e6b8ab7b4955bc0bcc826
Joseph Osborne <josephosborne@zap-map.com>
2025-07-05 10:45:20 +0100

    draft: turn on nerdfont

--- init.lua
+++ init.lua
@@ -95,7 +95,7 @@ vim.g.maplocalleader = ' '
 vim.g.R_assign = 3
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
